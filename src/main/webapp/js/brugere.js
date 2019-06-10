3ee270d42c3fe8922299502654c22b6b2dd96c8c
aleksanderlj <aleksanderlj@hotmail.com>
2019-06-10 15:29:18 +0200

    Startet på ajax/jersey // Aleksander

--- src/main/webapp/js/brugere.js
+++ src/main/webapp/js/brugere.js
@@ -1,3 +1,17 @@
 $(function(){
 
+    function ajaxCreate() {
+        jsondata = makeJSON()
+    }
+
+    function makeJSON(id, name, ini, cpr){
+        json = {
+            "id" : $('#user_id').val(),
+            "name" : $('#user_name').val(),
+            "ini" : $('#user_ini').val(),
+            "cpr" : $('#user_cpr').val()
+        };
+
+        return json;
+    }
 });

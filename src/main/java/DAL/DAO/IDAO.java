4daa33bf973137c2ad8b491391958fe934721509
Jonatan <s185096@student.dtu.dk>
2019-06-10 11:27:01 +0200

    Oprette packages og interfaces.

---
+++ src/main/java/DAL/DAO/IDAO.java
@@ -0,0 +1,26 @@
+package DAL.DAO;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface IDAO<E> {
+    int create(E objekt) throws DALException, SQLException;
+    E get(int id) throws DALException, SQLException;
+    List<E> getList() throws DALException, SQLException;
+    int update(E objekt) throws DALException, SQLException;
+    void delete(int id) throws DALException, SQLException;
+
+
+    public class DALException extends Exception {
+        //Til Java serialisering...
+        private static final long serialVersionUID = 7355418246336739229L;
+
+        public DALException(String msg, Throwable e) {
+            super(msg, e);
+        }
+        public DALException(String msg) {
+            super(msg);
+        }
+
+    }
+}

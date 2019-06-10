6255e4e7f9e0af394625c917e01eb3bc200b5c0d
Jonatan <s185096@student.dtu.dk>
2019-06-10 12:04:25 +0200

    Oprette DTO

---
+++ src/main/java/DAL/DAO/ProduktBatchDAO.java
@@ -0,0 +1,12 @@
+package DAL.DAO;
+
+import DAL.DTO.IProduktBatch;
+import java.sql.SQLException;
+
+public class ProduktBatchDAO implements IDAO {
+
+    @Override
+    public int create(IProduktBatch produktBatch) throws SQLException {
+        return 0;
+    }
+}

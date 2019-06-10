61e8ba9e1b7c0e981a11f85bc579aaaa2d263b0b
Jonatan <s185096@student.dtu.dk>
2019-06-10 13:56:08 +0200

    Tilføjet variabler til nogle DTO'er

--- src/main/java/DAL/DTO/RaavareBatch.java
+++ src/main/java/DAL/DTO/RaavareBatch.java
@@ -1,8 +1,48 @@
 package DAL.DTO;
 
 public class RaavareBatch {
+    private int raavareBatchId, raavareId;
+    private double maengde;
+    private String leverandoer;
 
-    public RaavareBatch(){
+    public RaavareBatch(){ }
 
+    public RaavareBatch(int raavareBatchId, int raavareId, double maengde, String leverandoer){
+        this.raavareBatchId = raavareBatchId;
+        this.raavareId = raavareId;
+        this.maengde = maengde;
+        this.leverandoer = leverandoer;
+    }
+
+    public int getRaavareBatchId() {
+        return raavareBatchId;
+    }
+
+    public void setRaavareBatchId(int raavareBatchId) {
+        this.raavareBatchId = raavareBatchId;
+    }
+
+    public int getRaavareId() {
+        return raavareId;
+    }
+
+    public void setRaavareId(int raavareId) {
+        this.raavareId = raavareId;
+    }
+
+    public double getMaengde() {
+        return maengde;
+    }
+
+    public void setMaengde(double maengde) {
+        this.maengde = maengde;
+    }
+
+    public String getLeverandoer() {
+        return leverandoer;
+    }
+
+    public void setLeverandoer(String leverandoer) {
+        this.leverandoer = leverandoer;
     }
 }

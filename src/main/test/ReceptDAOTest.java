8c33805fa7a245067ea20291d444f3a08456baef
Jonatan <s185096@student.dtu.dk>
2019-06-11 15:13:00 +0200

    Startet på receptDAOTest

--- src/main/test/ReceptDAOTest.java
+++ src/main/test/ReceptDAOTest.java
@@ -3,27 +3,39 @@ import java.sql.SQLException;
 import DAL.DAO.IDAO;
 import DAL.DAO.ReceptDAO;
 import DAL.DTO.Recept;
+import DAL.DTO.ReceptKomp;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 public class ReceptDAOTest {
     private ReceptDAO receptDAO = new ReceptDAO();
-    private Recept recept = new Recept();
+    ReceptKomp[] receptKompArray = new ReceptKomp[0];
+    private Recept recept = new Recept(7545, "Penicillin", receptKompArray);
     private Recept recivedRecept = new Recept();
 
     @Test
-    public void receptTest() throws SQLException, IDAO.DALException {
-        // Tester create i recept dao
+    public void createTest() throws SQLException, IDAO.DALException {
         receptDAO.delete(7545);
-        recept.setId(7545);
-        recept.setNavn("Penicillin");
         receptDAO.create(recept);
         recivedRecept = receptDAO.get(7545);
         assertEquals(recept.getId(), recivedRecept.getId());
         assertEquals(recept.getNavn(), recivedRecept.getNavn());
-
-
         receptDAO.delete(7545);
+    }
+
+    @Test
+    public void updateTest() throws SQLException, IDAO.DALException {
+        receptDAO.delete(7545);
+        receptDAO.create(recept);
+        recept.setNavn("Antibiotika");
+        receptDAO.update(recept);
+        assertEquals(recept.getNavn(), receptDAO.get(7545).getNavn());
+        receptDAO.delete(7545);
+    }
+
+    @Test
+    public void getListTest(){
+
     }
 }

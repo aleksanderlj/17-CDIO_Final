eb5d74dbfe0e31135ff05508d06e6d96be520f18
Jonatan <s185096@student.dtu.dk>
2019-06-12 09:45:50 +0200

    ReceptDAO er opdateret, til at indkludere receptKomp tabellen

--- src/main/java/DAL/DAO/ReceptDAO.java
+++ src/main/java/DAL/DAO/ReceptDAO.java
@@ -26,7 +26,7 @@ public class ReceptDAO implements IDAO<Recept> {
             statement.executeUpdate();
 
             statement = connection.prepareStatement
-                    ("INSERT INTO receptKomp (receptID, raavareID, nonNetto, tolerance) VALUES (?,?,?,?)");
+                    ("INSERT INTO receptKomp (receptID, raavareID, nonNetto, tolerance) VALUES (?,?,?,?);");
             for(int i = 0 ; i < recept.getIndholdsListe().length ; i++){
                 statement.setInt(1, recept.getId());
                 statement.setInt(2, recept.getIndholdsListe()[i].getRaavareId());
@@ -48,7 +48,6 @@ public class ReceptDAO implements IDAO<Recept> {
     public Recept get(int id) throws DALException, SQLException {
         Connection connection = connectionController.createConnection();
         Recept recept = new Recept();
-        recept.setId(id);
         List<ReceptKomp> indholdsListe = new ArrayList<>();
 
         try{
@@ -57,13 +56,14 @@ public class ReceptDAO implements IDAO<Recept> {
             PreparedStatement statement = connection.prepareStatement
                     ("SELECT * FROM recept WHERE receptID = ?;");
             statement.setInt(1, id);
+            recept.setId(id);
             ResultSet resultSet = statement.executeQuery();
             if (resultSet.next()){
                 recept.setNavn(resultSet.getString(2));
             }
 
             statement = connection.prepareStatement
-                    ("SELECT * FROM receptKomp WHERE receptID = ?");
+                    ("SELECT * FROM receptKomp WHERE receptID = ?;");
             resultSet = statement.executeQuery();
             while (resultSet.next()){
                 indholdsListe.add(new ReceptKomp());
@@ -86,7 +86,8 @@ public class ReceptDAO implements IDAO<Recept> {
         List<Recept> receptList = new ArrayList<>();
         Connection connection = connectionController.createConnection();
         Recept[] receptArray;
-        int lastElement;
+        List<ReceptKomp> indholdsListe;
+        ReceptKomp[] indholdsArray;
 
         try{
             connection.setAutoCommit(false);
@@ -94,11 +95,22 @@ public class ReceptDAO implements IDAO<Recept> {
             PreparedStatement statement = connection.prepareStatement
                     ("SELECT * FROM recept;");
             ResultSet resultSet = statement.executeQuery();
+            PreparedStatement statement1 = connection.prepareStatement
+                    ("SELECT * FROM receptKomp;");
+            ResultSet resultSet1 = statement1.executeQuery();
             while(resultSet.next()){
                 receptList.add(new Recept());
-                lastElement = receptList.size() - 1;
-                receptList.get(lastElement).setId(resultSet.getInt(1));
-                receptList.get(lastElement).setNavn(resultSet.getString(2));
+                receptList.get(receptList.size() - 1).setId(resultSet.getInt(1));
+                receptList.get(receptList.size() - 1).setNavn(resultSet.getString(2));
+                indholdsListe = new ArrayList<>();
+                while(resultSet1.next()){
+                    indholdsListe.add(new ReceptKomp());
+                    indholdsListe.get(indholdsListe.size()-1).setRaavareId(resultSet.getInt(2));
+                    indholdsListe.get(indholdsListe.size()-1).setNonNetto(resultSet.getDouble(3));
+                    indholdsListe.get(indholdsListe.size()-1).setTolerance(resultSet.getDouble(4));
+                }
+                indholdsArray = indholdsListe.toArray(new ReceptKomp[indholdsListe.size()]);
+                receptList.get(receptList.size() - 1).setIndholdsListe(indholdsArray);
             }
             connection.commit();
         }catch (SQLException e){
@@ -124,6 +136,22 @@ public class ReceptDAO implements IDAO<Recept> {
             statement.setInt(2,recept.getId());
             statement.executeUpdate();
 
+            statement = connection.prepareStatement
+                    ("DELETE FROM receptKomp WHERE receptID = ?;");
+            statement.setInt(1, recept.getId());
+            statement.executeUpdate();
+
+            statement = connection.prepareStatement
+                    ("INSERT INTO receptKomp (receptID, raavareID, nonNetto, tolerance) VALUES (?,?,?,?);");
+            for(int i = 0 ; i < recept.getIndholdsListe().length ; i++){
+                statement.setInt(1, recept.getId());
+                statement.setInt(2, recept.getIndholdsListe()[i].getRaavareId());
+                statement.setDouble(3, recept.getIndholdsListe()[i].getNonNetto());
+                statement.setDouble(4, recept.getIndholdsListe()[i].getTolerance());
+                statement.executeUpdate();
+            }
+
+
             connection.commit();
         }catch (SQLException e){
             e.printStackTrace();
@@ -142,6 +170,10 @@ public class ReceptDAO implements IDAO<Recept> {
             PreparedStatement statement = connection.prepareStatement
                     ("DELETE FROM recept WHERE receptID = ?;");
             statement.setInt(1,id);
+            statement.executeUpdate();
+
+            statement = connection.prepareStatement("DELETE FROM receptKomp WHERE receptID = ?;");
+            statement.setInt(1, id);
             statement.executeUpdate();
 
             connection.commit();

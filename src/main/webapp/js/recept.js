106e05dd42818ab1b32f8cbebdbc38f482fe78f6
Your Name <you@example.com>
2019-06-12 15:47:23 +0200

    opbegyndt tilføjknap --Søren

--- src/main/webapp/js/recept.js
+++ src/main/webapp/js/recept.js
@@ -1,3 +1,18 @@
 $(function(){
 
+
+
+    /*Tilføj række*/
+    function addrow(data){
+
+        var rowCount = table.rows.length;
+        var row = table.insertRow(1);
+
+        row.insertCell(0).innerHTML = data.id;
+        row.insertCell(1).innerHTML = data.navn;
+        row.insertCell(2).innerHTML = data.ini;
+        row.insertCell(3).innerHTML = data.cpr;
+        row.insertCell(4).innerHTML = aktiv;
+
+    }
 });

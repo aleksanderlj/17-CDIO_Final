68656e251df1ca32befdafe49db9a00e2538b5da
Your Name <you@example.com>
2019-06-13 13:40:54 +0200

    næsten tilføjknap
    --Søren

--- src/main/webapp/js/recept.js
+++ src/main/webapp/js/recept.js
@@ -1,18 +1,34 @@
 $(function(){
 
+    $('#recept_form').submit(function(e) {
+        e.preventDefault();
+        addIndholdRow();
+    });
 
 
-    /*Tilføj række*/
-    function addrow(data){
+    $('#tilføjknap').onclick(function () {
+        addIndholdRow();
+    })
+
+    /*Tilføj række af indholdsstoffer*/
+    function addIndholdRow(){
+        var table = document.getElementById("opretRecept2");
 
         var rowCount = table.rows.length;
-        var row = table.insertRow(1);
 
-        row.insertCell(0).innerHTML = data.id;
-        row.insertCell(1).innerHTML = data.navn;
-        row.insertCell(2).innerHTML = data.ini;
-        row.insertCell(3).innerHTML = data.cpr;
-        row.insertCell(4).innerHTML = aktiv;
+        var row = table.insertRow(rowCount);
 
+        row.insertCell(0).innerHTML = $('#comp_raavare').val();
+        row.insertCell(1).innerHTML = $('#comp_amount').val();
+        row.insertCell(2).innerHTML = $('#comp_tolerance').val();
+        row.insertCell(3).innerHTML = $('#comp_tolerance').val();
+    }
+
+    function makeRemoveRowBtn(id) {
+        var removeBtn = document.createElement('remove');
+
+        removeBtn.type = "button";
+        removeBtn.name = "removeRowBtn"
+        removeBtn.onclick = (function() {removeRow})
     }
 });

25c4bf5413cb140970e7f0f9e07495c9fc9845fc
Your Name <you@example.com>
2019-06-13 15:43:50 +0200

    Tilføj knap og fjern knap done

--- src/main/webapp/js/recept.js
+++ src/main/webapp/js/recept.js
@@ -1,34 +1,41 @@
-$(function(){
+$(function () {
 
-    $('#recept_form').submit(function(e) {
+    $('#recept_form').submit(function (e) {
         e.preventDefault();
-        addIndholdRow();
     });
 
 
-    $('#tilføjknap').onclick(function () {
+    $('#addrowbtn').click(function (e) {
         addIndholdRow();
+        document.getElementById('comp_raavare').value = '';
+        document.getElementById('comp_amount').value = '';
+        document.getElementById('comp_tolerance').value = '';
     })
 
     /*Tilføj række af indholdsstoffer*/
-    function addIndholdRow(){
+    function addIndholdRow() {
         var table = document.getElementById("opretRecept2");
-
-        var rowCount = table.rows.length;
-
-        var row = table.insertRow(rowCount);
+        var row = table.insertRow(2);
 
         row.insertCell(0).innerHTML = $('#comp_raavare').val();
         row.insertCell(1).innerHTML = $('#comp_amount').val();
         row.insertCell(2).innerHTML = $('#comp_tolerance').val();
-        row.insertCell(3).innerHTML = $('#comp_tolerance').val();
+        row.insertCell(3).appendChild(makeRemoveRowBtn());
     }
 
-    function makeRemoveRowBtn(id) {
-        var removeBtn = document.createElement('remove');
-
+    function makeRemoveRowBtn() {
+        var removeBtn = document.createElement('input');
         removeBtn.type = "button";
-        removeBtn.name = "removeRowBtn"
-        removeBtn.onclick = (function() {removeRow})
+        removeBtn.name = "removeRowBtn";
+        removeBtn.value = "Fjern";
+        removeBtn.onclick = (function() {removeRow(this)});
+        return removeBtn;
     }
+
+    function removeRow(obj) {
+        var index = obj.parentNode.parentNode.rowIndex;
+        var table = document.getElementById("opretRecept2");
+        table.deleteRow(index);
+    }
+
 });

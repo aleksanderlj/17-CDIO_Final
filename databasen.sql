f11362dedcf94613491b023b4beffedd2a508165
Your Name <you@example.com>
2019-06-14 11:20:29 +0200

    not null i databasen

--- databasen.sql
+++ databasen.sql
@@ -1,5 +1,4 @@
 USE s185103;
-drop table raavare;
 CREATE TABLE raavare(
 		raavareID INT PRIMARY KEY, 
 		raavareNavn TEXT NOT NULL);
@@ -11,18 +10,18 @@ CREATE TABLE recept(
 CREATE TABLE receptKomp(
 		receptID INT,
         raavareID INT,
-        nonNetto DOUBLE(7,4),
-        tolerance DOUBLE(3,1),
+        nonNetto DOUBLE(7,4) NOT NULL,
+        tolerance DOUBLE(3,1) NOT NULL,
         FOREIGN KEY (receptID) REFERENCES recept(receptID),
         FOREIGN KEY (raavareID) REFERENCES raavare(raavareID));
-        
+
 CREATE TABLE raavareBatch(
 		raavareBatchID INT PRIMARY KEY, 
 		raavareID INT, 
-		maengde DOUBLE(7,4),
-		leverandoer TEXT,
+		maengde DOUBLE(7,4) NOT NULL,
+		leverandoer TEXT NOT NULL,
 		FOREIGN KEY (raavareID) REFERENCES raavare(raavareID));
-        
+
 CREATE TABLE produktBatch(
 		produktBatchID INT PRIMARY KEY AUTO_INCREMENT, 
 		receptID INT, 
@@ -31,13 +30,11 @@ CREATE TABLE produktBatch(
         slutDato TEXT,
         FOREIGN KEY (receptID) REFERENCES recept(receptID));
         
-drop table produktBatchKomp, produktBatch, raavareBatch, receptKomp;
-
 CREATE TABLE produktBatchKomp(
 		produktBatchID INT,
         raavareBatchID INT,
-        tara DOUBLE(7,4), 
-        netto DOUBLE(7,4),
+        tara DOUBLE(7,4) NOT NULL, 
+        netto DOUBLE(7,4) NOT NULL,
         brugerID INT,
         FOREIGN KEY (produktBatchID) REFERENCES produktBatch(produktBatchID),
         FOREIGN KEY (raavareBatchID) REFERENCES raavareBatch(raavareBatchID),
@@ -45,11 +42,11 @@ CREATE TABLE produktBatchKomp(
 
 CREATE TABLE bruger(
 		brugerID INT PRIMARY KEY,
-        brugerNavn TEXT,
-        ini TEXT,
-        cpr TEXT,
+        brugerNavn TEXT NOT NULL,
+        ini TEXT NOT NULL,
+        cpr TEXT NOT NULL,
         aktiv BOOLEAN);
-        
+       
 insert into bruger set brugerID = 1, brugerNavn = "Simon", ini ="SH", cpr = "100496", aktiv = true;
 insert into bruger set brugerID = 2, brugerNavn = "Silas", ini ="SJ", cpr = "241097", aktiv = true;
 insert into bruger set brugerID = 3, brugerNavn = "Peter", ini ="PH", cpr = "150700", aktiv = true;
